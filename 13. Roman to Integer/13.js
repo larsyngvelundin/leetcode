adf0682fd758727ad138b2a1c0e5eff2857e6545
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2023-01-23 23:16:56 +0100

    13 Fixed bug
    Bug regarding Roman numbers that are modifiers as well as larger than the next number, resulting in them not being added to the result.

--- 13. Roman to Integer/13.js
+++ 13. Roman to Integer/13.js
@@ -1,5 +1,5 @@
-let testCases = ["III", "LVIII", "MCMXCIV", "IX", "XC"];
-let expectedResults = [3, 58, 1994, 9, 90];
+let testCases = ["III", "LVIII", "MCMXCIV", "IX", "XC", "DCXXI"];
+let expectedResults = [3, 58, 1994, 9, 90, 621];
 
 
 
@@ -13,11 +13,12 @@ let modifiers = ["I", "X", "C"];
 var romanToInt = function (s) {
     let result = 0;
     for (let i = 0; i < s.length; i++) {
+        console.log(symbolLookup[s[i]]);
         if (i > 0 && modifiers.includes(s[i - 1]) && symbolLookup[s[i]] > symbolLookup[s[i - 1]]) {
             result += symbolLookup[s[i]] - symbolLookup[s[i - 1]];
         }
         else if (modifiers.includes(s[i]) && i < s.length - 1) {
-            if (symbolLookup[s[i]] == symbolLookup[s[i + 1]]) {
+            if (symbolLookup[s[i]] >= symbolLookup[s[i + 1]]) {
                 result += symbolLookup[s[i]];
             }
         }
