c4c3b69a7a35c8794658fd7c090d63735f279ba8
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2023-03-14 10:04:30 +0100

    Create 58.java

---
+++ 58. Length of Last Word/58.java
@@ -0,0 +1,19 @@
+// Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+// A word is a maximal substring consisting of non-space characters only.
+
+class Solution {
+
+    public static void main(String[] args) {
+        private String[] testCases = new String[4];
+        System.out.println("========================");
+        System.out.println("Start of code");
+        System.out.println("========================");
+        int result = lengthOfLastWord("test");
+        System.out.println(result);
+    }
+
+    static int lengthOfLastWord(String s) {
+        return 0;
+    }
+}
