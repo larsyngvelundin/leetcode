90888f17150b634472eb0ec7566876942f7f17c7
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2023-09-28 18:20:26 +0200

    66 Optimized

--- 66. Plus One/66.py
+++ 66. Plus One/66.py
@@ -1,39 +1,31 @@
 test_cases = [
-    [1,2,3],
-    [4,3,2,1],
+    [1, 2, 3],
+    [4, 3, 2, 1],
     [9],
-    [5,9,9],
-    [9,9],
-    [9,8,9]
+    [5, 9, 9],
+    [9, 9],
+    [9, 8, 9]
 ]
 
 expected_results = [
-    [1,2,4],
-    [4,3,2,2],
-    [1,0],
-    [6,0,0],
-    [1,0,0],
-    [9,9,0]
+    [1, 2, 4],
+    [4, 3, 2, 2],
+    [1, 0],
+    [6, 0, 0],
+    [1, 0, 0],
+    [9, 9, 0]
 ]
 
 
 class Solution:
     def plusOne(self, digits):
-        last = len(digits)-1
-        if digits[last] != 9:
-            digits[last] += 1
-        else:
-            for i in range(last, -1, -1):
-                if digits[i] == 9:
-                    digits[i] = 0
-                else:
-                    digits[i] += 1
-                    break
-        if digits[0] == 0:
-            digits[0] = 1
-            digits.append(0)
-        return digits
-
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                return digits
+        return [1] + digits
 
 
         # Testing
